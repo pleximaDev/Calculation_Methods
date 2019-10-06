1d9208e004f6e19371b6c3fe32fefb24ecfda6df
pleximaDev <drscrut@gmail.com>
2019-10-06 13:51:29 +0300

    lab_decomposition.m

--- lab_decomposition.m
+++ lab_decomposition.m
@@ -26,7 +26,6 @@ L = D;
 Q_inv = Q^(-1);
 
 Ans = Q*Q_inv;
-(Ans < 0.8);
 Ans(Ans < 0.8) = 0;
 
 disp(A)
@@ -95,6 +94,7 @@ fprintf("_____________________")
 A
 L*U
 
+
 fprintf("_____________________\n")
 [L,U] = my_lu(A, "Crout")
 A
@@ -105,12 +105,17 @@ fprintf("_____________________\n")
 
 [my, p] = my_chol(A, "Cholesky_Banachiewicz")
 [standard] = chol(A, 'lower')
+asymmentric_matrix = [1.4, 1, 2; 1, 0.9, 1; 1, 1, 1.4]
+%%% Cholesky for asymmetric matrix %%%
+[L_asymm, ok] = my_chol(asymmentric_matrix, "Cholesky_Banachiewicz")
+B = L_asymm * conj(L_asymm)'
+%%%               //               %%%
 
-
-
-
-[Q,R] = qr(A);
-[Q, R] = my_qr(A);
+[Q_std, R_std] = qr(A)
+[Q_classical, R] = my_qr(A, "Classical Gram_Schmidt")
+[Q_modified, R] = my_qr(A, "Modified Gram_Schmidt")
+[Q_Householder, R] = my_qr(A, "Householder")
+[Q_Givens, R] = my_qr(A, "Givens")
 
 
 function [L,U] = my_lu(x, method)
@@ -125,11 +130,13 @@ switch method
             for j = i : 1 : n
                 for k = 1 : 1 : i-1
                     sumU = sumU + L(i, k) * U(k, j);
-                    sumL = sumL + L(j, k) * U(k, i);
                 end
                 U(i,j) = x(i,j) - sumU;
                 sumU = 0;
                 L(i,i) = 1;
+                for k = 1 : 1 : i-1
+                    sumL = sumL + L(j, k) * U(k, i);
+                end
                 if j ~= n
                     j = j + 1;
                 end
@@ -212,10 +219,11 @@ end
 
 function [Q,R] = my_qr(x, method)
 switch method
-    case "Classical Gram–Schmidt"
+    case "Classical Gram_Schmidt"
         [m,n] = size(x);
         Q = zeros(m,n);
         sum_proj = zeros(m,1);
+        proj = @(x, Q) ((x(:,k)' * Q(:,j))/(Q(:,j)'*Q(:,j))).*Q(:,j);
 
         for k = 1 : 1 : m
             for j = 1 : 1 : k-1
@@ -232,7 +240,7 @@ switch method
         R(logic) = 0;
         %___________Unnecessary___________%
         
-    case "modified Gram–Schmidt"
+    case "Modified Gram_Schmidt"
         [m,n] = size(x);
         Q = zeros(m, n);
         b = zeros(m, n);
@@ -256,29 +264,72 @@ switch method
         logic = (zero_logic == 0);
         R(logic) = 0;
         %___________Unnecessary___________%
+    case "Householder"
+        % Reflections
+        A = x;
+        [m, n] = size(A);
+        Q = eye(m, n);
+        e = zeros(m, 1);
+        
 
+        for k = 1 : 1 : n-1
+            e(k, 1) = 1;
+            x = A(:, k);
+            for i = 1 : 1 : k-1
+                x(i, 1) = 0;
+            end
+            u = x - norm(x) * e;
+            P = eye(m, n) - 2 * (u * u')/(norm(u))^2;
+            Q = Q * P;
+            A = P * A;
+            R = A;
+            e(e>1e-12) = 0;
+        end
+        % <changing signs>
+        R = -R;
+        Q = -Q;
+        % <\changing signs>
+        %___________Unnecessary___________%
+        zero_logic = triu(ones(m,n));
+        logic = (zero_logic == 0);
+        R(logic) = 0;
+        %___________Unnecessary___________%
+        
+    case "Givens"
+        % Rotations
+        [m,n] = size(x);
+        A = x;
+        Q = eye(m, n);
+        R = A;
+        G = eye(n, n);
+        for i = 1 : 1 : n - 1
+            for j = i + 1 : 1 : n
+                G = eye(n, n);
+%                 fprintf("ij(%d, %d)\n", i, j);
+                G(i, i) = A(i, i)/sqrt(A(i, i)^2 + A(j, i)^2);
+                G(j, j) = G(i,i);
+                G(i, j) = A(j, i)/sqrt(A(i, i)^2 + A(j, i)^2);
+                G(j, i) = -A(j, i)/sqrt(A(i, i)^2 + A(j, i)^2);
+                Q = Q * G';
+                A = G * R;
+                R = A;
+            end
+        end
+        % <changing signs>
+        R = -R;
+        Q = -Q;
+        % <\changing signs>
+        %___________Unnecessary___________%
+        zero_logic = triu(ones(m,n));
+        logic = (zero_logic == 0);
+        R(logic) = 0;
+        %___________Unnecessary___________%
     otherwise 
         fprintf("Error occured while entering method's name.");
 end
 end
 
-%previous tries
 
-% function [Q,R] = my_modified_qr(x)
-% [m,n] = size(x);
-% Q = zeros(m,n);
-% R = zeros(m,n);
-% 
-% Q(:,1) = x(:, 1)/norm(x(:, 1));
-% for k = 2 : 1 : m
-%     Q(:,k - 1)
-%     b_k = Q(:,k - 1)/norm(Q(:,k - 1))
-%     proj = ((Q(:,k - 1)' * b_k)/(b_k'*b_k)).*b_k
-%     Q(:,k) = Q(:, k - 1) - proj;
-%     break
-%     Q(:,k) = -Q(:,k)/norm(Q(:,k));
-% end
-% end
 
 
 
